291ede269778340928421c14cfa84f4710024da4
Shubham Bakhal <shubham@azodha.com>
2023-05-13 19:48:02 +0530

    added db url

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,11 +1,14 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { body, validationResult } from "express-validator";
+import { checkJwt } from "../middleware/auth0";
 import * as UserService from "../services/user.service";
 
 const router = Router();
 
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
     const { email } = req.query;
+    console.log(email);
+
     if (email) {
         try {
             const user = await UserService.getUserByEmail(email as string);
@@ -23,7 +26,7 @@ router.get("/", async (req: Request, res: Response, next: NextFunction) => {
     }
 });
 
-router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
+router.get("/:id", checkJwt, async (req: Request, res: Response, next: NextFunction) => {
     try {
         const user = await UserService.getUser(parseInt(req.params.id, 10));
         if (user) {
@@ -39,6 +42,7 @@ router.post(
     "/",
     body("email").isEmail(),
     body("firstName").isString().isLength({ min: 1 }),
+    checkJwt,
     async (req: Request, res: Response, next: NextFunction) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -75,7 +79,7 @@ router.put(
     },
 );
 
-router.delete("/:id", async (req: Request, res: Response, next: NextFunction) => {
+router.delete("/:id", checkJwt, async (req: Request, res: Response, next: NextFunction) => {
     try {
         // req.user.id = parseInt(req.params.id, 10);
         // else throw
